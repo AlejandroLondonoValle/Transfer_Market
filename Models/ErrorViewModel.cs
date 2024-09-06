70b4d7c6104cd6f859d76976ea71162de9fb2a85
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-09-06 07:26:38 -0500

    feat: Se inicaliza el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TransferMarket.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
