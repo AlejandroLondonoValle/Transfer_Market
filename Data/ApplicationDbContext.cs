b030ac422cc1f6ad7b49fc3e269b702cd716eb18
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-09-06 07:42:09 -0500

    feat: se crea estructura inicial del proyecto y se conecta a la db

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TransferMarket.Models;
+using System.Security.Cryptography;
+
+namespace TransferMarket.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+
+    public DbSet<Jugador> Jugadores { get; set; }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
+using DotNetEnv;
+using TransferMarket.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+Env.Load();
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var mySqlConnection = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection,ServerVersion.Parse("8.0.20-mysql")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
